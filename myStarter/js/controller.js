c083a707d8abfc580d3b25cb7c42a08a0e76d152
Angelina-K <79867604+Angelina-K@users.noreply.github.com>
2021-11-03 13:24:00 +0200

    small changes

--- myStarter/js/controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-// import{} from './service/app-service.js'
-window.onInit = onInit;
-
-function onInit() {
-  console.log('onInit');
-}
--- myStarter/js/service/app-service.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-import { storageService } from './storage-service.js';
-// export const service={
-// }
-
-function _saveToStorage() {
-  // saveToStorage('DB', gBooks);
-  storageService.save();
-}
