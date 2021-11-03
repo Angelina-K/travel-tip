8e7e138323aaccec9dd9882edca313af554721d0
adamberco <adamberco@gmail.com>
2021-11-03 19:31:56 +0200

    added search and get addres from map

--- js/app.controller.js
+++ js/app.controller.js
@@ -9,62 +9,69 @@ window.onGetUserPos = onGetUserPos;
 window.onMapClick = onMapClick;
 window.onGoToSaved = onGoToSaved;
 window.onSearch = onSearch;
+window.onRemoveSaved = onRemoveSaved
+
 
 function onInit() {
-  mapService
-    .initMap()
-    .then(() => {
-      console.log('Map is ready');
-    })
-    .catch(() => console.log('Error: cannot init map'));
+    mapService
+        .initMap()
+        .then(() => {
+            console.log('Map is ready');
+        })
+        .catch(() => console.log('Error: cannot init map'));
 }
 
 // This function provides a Promise API to the callback-based-api of getCurrentPosition
 function getPosition() {
-  console.log('Getting Pos');
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
+    console.log('Getting Pos');
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
 }
 
 function onAddMarker(lat, lng) {
-  console.log('Adding a marker');
-  mapService.addMarker({ lat, lng });
+    console.log('Adding a marker');
+    mapService.addMarker({ lat, lng });
 }
 
 function onGetLocs() {
-  locService.getLocs().then(renderLocs);
+    locService.getLocs().then(renderLocs);
 }
 
 function onGetUserPos() {
-  getPosition()
-    .then((pos) => {
-      const lat = pos.coords.latitude;
-      const lng = pos.coords.longitude;
-      mapService.panTo(lat, lng);
-      mapService.addMarker({ lat, lng });
-    })
-    .catch((err) => {
-      console.log('err!!!', err);
-    });
+    getPosition()
+        .then((pos) => {
+            const lat = pos.coords.latitude;
+            const lng = pos.coords.longitude;
+            mapService.panTo(lat, lng);
+            mapService.addMarker({ lat, lng });
+        })
+        .catch((err) => {
+            console.log('err!!!', err);
+        });
 }
 function onPanTo(lat, lng) {
-  mapService.panTo(lat, lng);
+    mapService.panTo(lat, lng);
 }
 
 function onSearch() {
-  const elInput = document.querySelector('input[type="search"]');
-  const input = elInput.value;
+    const elInput = document.querySelector('input[type="search"]');
+    const input = elInput.value;
+    let pos
+    locService.getLocBySearch(input)
+        .then(res => {
+            onMapClick(null, res.lat, res.lng, input)
+        })
 }
 
 function onCopyLoc() {
-  console.log('onCopyLoc');
+    console.log('onCopyLoc');
 }
 
 function renderLocs(locs) {
-  const headerStr = `<h4>Locations:</h4>`;
-  const strHtml = locs.map((loc, idx) => {
-    const strLoc = `<div class="loc loc${idx} flex align-center space-between">
+    const headerStr = `<h4>Locations:</h4>`;
+    const strHtml = locs.map((loc, idx) => {
+        const strLoc = `<div class="loc loc${idx} flex align-center space-between">
       <div class="flex align-center gap">
       <h3>${loc.name}</h3>
       <span>Created At: ${loc.createdAt} <span>
@@ -75,36 +82,34 @@ function renderLocs(locs) {
       <button onclick="onRemoveSaved(${idx})" class="btn remove-btn">Delete</button>
       </div>
       </div>`;
-    return strLoc;
-  });
-  const elLocs = document.querySelector('.locs-container');
-  elLocs.innerHTML = headerStr + strHtml.join('');
+        return strLoc;
+    });
+    const elLocs = document.querySelector('.locs-container');
+    elLocs.innerHTML = headerStr + strHtml.join('');
 }
 
 function onGoToSaved(lat, lng) {
-  console.log(lat, lng);
-  console.log('onGoToSaved');
-  mapService.panTo(lat, lng);
-}
-function onDeleteSaved(idx) {
-  locService.removeLoc(idx);
-}
-function onMapClick(e) {
-  const locName = prompt('Cool place! How should we call it?');
-  const lat = e.latLng.lat();
-  const lng = e.latLng.lng();
-  onAddMarker(lat, lng);
-  onPanTo(lat, lng);
-  locService.createLoc(locName, lat, lng);
-  // saveToStorage('placesDB', gPlacesNames)
-  // renderPlaces()
+    console.log(lat, lng);
+    console.log('onGoToSaved');
+    mapService.panTo(lat, lng);
 }
 
-function placeMarkerAndPanTo(latLng, map) {
-  let marker = new google.maps.Marker({
-    position: latLng,
-    map: map,
-  });
-  map.panTo(latLng);
-  return { marker };
+function onRemoveSaved(idx) {
+    locService.removeLoc(idx);
+}
+function onMapClick(e, lat, lng, address) {
+    const locName = prompt('Cool place! How should we call it?');
+    if (!locName) return
+    lat = lat || e.latLng.lat()
+    lng = lng || e.latLng.lng()
+    onAddMarker(lat, lng)
+    onPanTo(lat, lng)
+    if (!address) {
+        locService.getAddress(lat, lng)
+            .then(res => {
+                locService.createLoc(locName, lat, lng, res)
+            })
+    } else locService.createLoc(locName, lat, lng, address)
+    // saveToStorage('placesDB', gPlacesNames)
+    // renderPlaces()
 }
--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,9 +1,12 @@
 import { storageService } from './storage-service.js';
 
+
 export const locService = {
     getLocs,
     createLoc,
-    removeLoc
+    removeLoc,
+    getLocBySearch,
+    getAddress
 }
 const locs_KEY = 'locsDB'
 const locs = storageService.load(locs_KEY) || []
@@ -21,18 +24,20 @@ function getLocs() {
     });
 }
 
-function createLoc(name, lat, lng) {
-    const time = new Date()
+function createLoc(name, lat, lng, address) {
+    const time = new Date().toLocaleString();
     const loc = {
         id: nextId++,
         name,
         lat,
         lng,
+        address,
         weather: null,
         createdAt: time,
         updatedAt: time,
     }
     console.log('adding new loc..');
+    console.log('loc', loc);
     locs.push(loc)
     storageService.save(locs_KEY, locs)
 }
@@ -42,3 +47,21 @@ function removeLoc(idx) {
     storageService.save(locs_KEY, locs)
 }
 
+function getLocBySearch(input) {
+    input = input.replace(' ', '+')
+    console.log('input', input)
+    return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${input}&key=AIzaSyAxYW_FjuUgnuOXRQY7Re8yde2lPtht1Qk`)
+        .then(res => {
+            console.log('res', res)
+            return res.data.results[0].geometry.location
+        })
+}
+
+function getAddress(lat, lng) {
+    console.log('lat, lng', lat, lng)
+    return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyAxYW_FjuUgnuOXRQY7Re8yde2lPtht1Qk`)
+        .then(res => {
+            return res.data.results[0].formatted_address
+        })
+
+}
