b3d5d502e19c07c2ce0ddaf3d503d3540df33264
Angelina-K <79867604+Angelina-K@users.noreply.github.com>
2021-11-03 23:40:16 +0200

    commit

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,63 +1,69 @@
 import { storageService } from './storage-service.js';
 
 export const locService = {
-    getLocs,
-    createLoc,
-    removeLoc,
-    getLocBySearch,
-    getAddress,
-    getwWeather
-}
-const locs_KEY = 'locsDB'
-const locs = storageService.load(locs_KEY) || []
+  getLocs,
+  createLoc,
+  removeLoc,
+  getLocBySearch,
+  getAddress,
+  // getwWeather
+};
+const locs_KEY = 'locsDB';
+const locs = storageService.load(locs_KEY) || [];
 
-let nextId = 101
+let nextId = 101;
 
 function getLocs() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(locs);
-        }, 2000)
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(locs);
+    }, 2000);
+  });
 }
 
 function createLoc(name, lat, lng, address) {
-    const time = new Date().toLocaleString();
-    const loc = {
-        id: nextId++,
-        name,
-        lat,
-        lng,
-        address,
-        weather: null,
-        createdAt: time,
-        updatedAt: time,
-    }
-    console.log('adding new loc..');
-    console.log('loc', loc);
-    locs.push(loc)
-    storageService.save(locs_KEY, locs)
+  const time = new Date().toLocaleString();
+  const loc = {
+    id: nextId++,
+    name,
+    lat,
+    lng,
+    address,
+    weather: null,
+    createdAt: time,
+    updatedAt: time,
+  };
+  console.log('adding new loc..');
+  console.log('loc', loc);
+  locs.push(loc);
+  storageService.save(locs_KEY, locs);
 }
 
 function removeLoc(idx) {
-    locs.splice(idx, 1)
-    storageService.save(locs_KEY, locs)
+  locs.splice(idx, 1);
+  storageService.save(locs_KEY, locs);
 }
 
 function getLocBySearch(input) {
-    input = input.replace(' ', '+')
-    console.log('input', input)
-    return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${input}&key=AIzaSyAxYW_FjuUgnuOXRQY7Re8yde2lPtht1Qk`)
-        .then(res => {
-            return res.data.results[0].geometry.location
-        })
+  input = input.replace(' ', '+');
+  console.log('input', input);
+  return axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${input}&key=AIzaSyAxYW_FjuUgnuOXRQY7Re8yde2lPtht1Qk`
+    )
+    .then((res) => {
+      return res.data.results[0].geometry.location;
+    });
 }
 
 function getAddress(lat, lng) {
-    return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyAxYW_FjuUgnuOXRQY7Re8yde2lPtht1Qk`)
-        .then(res => {
-            return res.data.results[0].formatted_address
-        })
+  return axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyAxYW_FjuUgnuOXRQY7Re8yde2lPtht1Qk`
+    )
+    .then((res) => {
+      return res.data.results[0].formatted_address;
+    });
 }
 
 // function getwWeather(lat, lng) {
